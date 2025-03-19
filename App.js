8ed3160f60c6234c21c7abfef7ff73262ba6f4d8
Jenifer Vieira <168489623+JeniferSilvaVieira@users.noreply.github.com>
2025-03-19 17:24:25 -0300

    linksbaixados

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import Vethome from './Screens/home';
 import {View, StyleSheet} from 'react-native';
+// Only import react-native-gesture-handler on native platforms
+import 'react-native-gesture-handler';
 
 export default function App() {
   return (
