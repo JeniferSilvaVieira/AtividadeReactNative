3144aaa67c236feeae9f4cf225f9efc89dd0e153
Jenifer Vieira <168489623+JeniferSilvaVieira@users.noreply.github.com>
2025-04-23 17:27:16 -0300

    aa

--- components/card.js
+++ components/card.js
@@ -3,9 +3,9 @@ import {View, Text, StyleSheet, Image} from 'react-native';
 export default function Card({nome, valor, img}) {
     return (
         <View style={styles.card}>
+            <Image source={{uri: img}} style={styles.imagem}></Image><Image/>
             <Text style={styles.text2}>{nome}</Text>
             <Text style={styles.text2}>R$ {valor}</Text>
-            <Image source={{uri: img}} style={styles.imagem}></Image><Image/>
         </View>
     )
 }
@@ -16,17 +16,14 @@ const styles = StyleSheet.create({
         margin: 10,
         backgroundColor: 'rgb(202, 120, 81)',
         borderRadius: 8,
-        alignItems: 'center'
+        alignItems: 'center',
+        flexDirection:"row",
     },
     text2: {
         fontSize: 20,
-        flexDirection: 'row',
-        gap: 5
     },
     imagem: {
         height: 150,
         width: 150,
-        flexDirection: 'row',
-        gap: 5
     }
 })
